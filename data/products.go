d2fda1299601fc100f3143d5b232757a61fa6d9f
Yeon Woo Park <ywoo1234@gmail.com>
2021-08-23 22:43:16 -0400

    Add Product structure with sample product list

---
+++ data/products.go
@@ -0,0 +1,37 @@
+package data
+
+import "time"
+
+// structure for API product
+type Product struct {
+	ID          int // unique ID
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+// create sample product list
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
